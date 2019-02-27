e129b2bba07e3e97227e9cf025f3dd15012af7fe
sks3a09 <sks3a09@gmail.com>
2019-02-27 23:00:36 +0800

    use arrays of function pointers

--- main.c
+++ main.c
@@ -5,19 +5,17 @@ int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
+int (*func[4])(int, int) = {plus, minus, multiply, divided};
 int main()
 {
-	int a, c;
+	int a, c, i;
 	char b;
 	printf("key a Function \nEX: 1 + 1\n");
 	scanf("%i %c %i", &a, &b, &c);
-	//IF ELSE IF
-	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
-	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
-	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
-	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+	char temp[] = "+-*/";
+	for(i=0;i<4;i++){
+		if(temp[i] == b)
+			printf("%d %c %d = %d\n", a, b, c, func[i](a, c));
+	}
 }
+
